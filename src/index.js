847c5d0abe961aca7ca09f3eee22a32331c7fce6
Rathees-Koneswarann <ratheesreo@gmail.com>
2023-03-13 01:40:50 +0530

    update components

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
 import './index.css'
 import App from './App'
 
@@ -7,14 +8,11 @@ import './api/server'
 
 import store from './store'
 
-store.dispatch({ type: 'todos/todoAdded', payload: 'Learn about actions' })
-// log: '1'
-// log: '2'
-// log: '3'
-
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 )
